b226479468d55132b85a6e2ca24ad580dbe06e42
JonahM-coder <jonahm@cpp.edu>
2024-01-22 17:11:09 -0800

    Added the functionality of spawning more than one hoverboard
    An inefficient process but what other approach could I use? Need to add models and implement functionality into main menu as well

---
+++ Assets/Scripts/NewLevelSelect.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NewLevelSelect : MonoBehaviour
+{
+    public string[] sceneList;
+    public int index = 2;
+    public Text sceneNameText;
+
+    private void Start()
+    {
+        index = PlayerPrefs.GetInt("SceneSelected", 0); // Default to the first scene
+        UpdateSceneNameText();
+    }
+
+    public void ToggleLeft()
+    {
+        ChangeScene(-1);
+        UpdateSceneNameText();
+    }
+
+    public void ToggleRight()
+    {
+        ChangeScene(1);
+        UpdateSceneNameText();
+    }
+
+    private void ChangeScene(int direction)
+    {
+        index = (index + direction + sceneList.Length) % sceneList.Length;
+    }
+
+    public void LoadScene()
+    {
+        PlayerPrefs.SetInt("SceneSelected", index);
+        SceneManager.LoadScene(sceneList[index], LoadSceneMode.Single);
+    }
+
+    private void UpdateSceneNameText()
+    {
+        sceneNameText.text = sceneList[index]; // Update the UI Text with the current scene name
+    }
+
+}
---
+++ Assets/Scripts/Prototype Scripts/CameraBehaviorSolo.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviorSolo : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject cameraTarget;
+    
+    public float speed;
+    public string playerTag;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+
+        FindPlayer();
+        
+        //player = GameObject.FindGameObjectWithTag("Player");
+        //cameraTarget = player.transform.Find("CameraTarget").gameObject;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Follow();
+    }
+
+    private void Follow()
+    {
+        gameObject.transform.position = Vector3.Lerp(transform.position, cameraTarget.transform.position, Time.deltaTime * speed);
+        gameObject.transform.LookAt(player.gameObject.transform.position);
+    }
+
+    private void FindPlayer()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag(playerTag);
+        if (players.Length > 0)
+        {
+            player = players[0];
+            cameraTarget = player.transform.Find("CameraTarget")?.gameObject;
+        }
+        else
+        {
+            Debug.LogError("No GameObject with tag '" + playerTag + "' found.");
+        }
+    }
+
+}
